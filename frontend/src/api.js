d14e4acabcd5c6924a332c4e686b03dc0b38ff6c
zoie <zoiedrizella@gmail.com>
2026-05-04 04:21:35 +0800

    PALDO KA ZOIE

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://palayguardgit-production.up.railway.app/api',
+  baseURL: 'http://localhost:3001/api',
 });
 
 API.interceptors.request.use((config) => {
